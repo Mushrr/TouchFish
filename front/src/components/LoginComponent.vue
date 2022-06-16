bc088d7970597938e807cf8ea15938f584471b66
HuangXingjie2002 <mushroom@mails.ccnu.edu.cn>
2022-06-16 12:29:33 +0800

    add shadow to login page

--- front/src/components/LoginComponent.vue
+++ front/src/components/LoginComponent.vue
@@ -6,15 +6,24 @@
     <div class="loginBox">
       <div class="username cantSelect">
         <label for="username">用户名:</label>
-        <input class="input-blue" type="text" name="username" id="username" v-model="user"/><br />
+        <input
+          class="input-blue"
+          type="text"
+          name="username"
+          id="username"
+          v-model="user"
+        /><br />
       </div>
       <div class="password cantSelect">
         <label for="password">密码:</label>
-        <input class="input-blue" type="password" name="password" id="password" />
+        <input
+          class="input-blue"
+          type="password"
+          name="password"
+          id="password"
+        />
       </div>
-      <button class="submitbtn cantSelect" @click="login">
-        submit
-      </button>
+      <button class="submitbtn cantSelect" @click="login">submit</button>
     </div>
   </div>
 </template>
@@ -31,9 +40,9 @@ export default {
   },
   methods: {
     login() {
-        // ... 登录
-    }
-  }
+      // ... 登录
+    },
+  },
 };
 </script>
 
@@ -63,39 +72,41 @@ export default {
 
   justify-items: center;
 
-
   width: 30vw;
   height: 200px;
   background-image: linear-gradient(to bottom right, #18c5ff, #19fffb, #18c5ff);
   border-radius: 10px;
   padding: var(--padding-box);
   font-family: var(--font-pingfang);
+  box-shadow: -1.1px -1.1px 23.4px rgba(0, 0, 0, 0.054),
+    1.5px 1.5px 44.3px rgba(0, 0, 0, 0.062);
+  transition: all 0.5s;
 }
+
 
 .username,
 .password {
-    display: column;
-    height: 30px;
-    margin-top: 15px;
-    flex-direction: row;
-    align-content: center;
+  display: column;
+  height: 30px;
+  margin-top: 15px;
+  flex-direction: row;
+  align-content: center;
 }
 
-
 .submitbtn {
-    font-family: 'PingFang';
-    font-weight: 500;
-    width: 50%;
-    height: 60%;
-    border-radius: 5px;
-    outline: none;
-    border: none;
+  font-family: "PingFang";
+  font-weight: 500;
+  width: 50%;
+  height: 60%;
+  border-radius: 5px;
+  outline: none;
+  border: none;
 }
 
 input {
-    outline: none;
-    padding: var(--padding-box);
-} 
+  outline: none;
+  padding: var(--padding-box);
+}
 
 @media screen and (max-width: 900px) {
   .loginBox {
